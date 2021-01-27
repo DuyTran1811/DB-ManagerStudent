d2460d572cafe0a3d36e747ed87ca38bc7ed3488
Duy Trần <duytran@Duys-MacBook-Pro.local>
2021-01-27 13:36:13 +0700

    fix

--- ManagerStudent.sql
+++ ManagerStudent.sql
@@ -54,24 +54,25 @@ INSERT INTO `ManagerStudent`.`Address`(`id`,`name`) VALUE('3', 'Tran Van C');
 INSERT INTO `ManagerStudent`.`Address`(`id`,`name`) VALUE('4', 'Tran Van D');
 INSERT INTO `ManagerStudent`.`Address`(`id`,`name`) VALUE('5', 'Tran Van E');
 
+
 INSERT INTO `ManagerStudent`.`Classes`(`id`,`name`,`language`,`description`) VALUE('1', 'Tran Van A','Java','Programer.OOP');
 INSERT INTO `ManagerStudent`.`Classes`(`id`,`name`,`language`,`description`) VALUE('2', 'Tran Van B','C++','Programer.OOP');
 INSERT INTO `ManagerStudent`.`Classes`(`id`,`name`,`language`,`description`) VALUE('3', 'Tran Van C','C#','Programer.OOP');
 INSERT INTO `ManagerStudent`.`Classes`(`id`,`name`,`language`,`description`) VALUE('4', 'Tran Van D','PHP','Programer.OOP');
 INSERT INTO `ManagerStudent`.`Classes`(`id`,`name`,`language`,`description`) VALUE('5', 'Tran Van E','Ruby','Programer.OOP');
 
+
 INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(1, 'Tran Van A',1,'0902123456',1);
 INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(2, 'Tran Van B',2,'0902123457',2);
 INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(3, 'Tran Van C',3,'0902123458',3);
 INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(4, 'Tran Van D',4,'0902123459',4);
 INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(5, 'Tran Van E',5,'0902123450',5);
+INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(6, 'Tran Van F',1,'0902123411',1);
+INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(7, 'Tran Van G',2,'0902123412',2);
+INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(8, 'Tran Van H',3,'0902123413',3);
+INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(9, 'Tran Van K',4,'0902123414',4);
+INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(10, 'Tran Van J',5,'0902123415',5);
 
-
-INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(6, 'Tran Van F',6,'0902123411',6);
-INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(7, 'Tran Van G',7,'0902123412',7);
-INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(8, 'Tran Van H',8,'0902123413',8);
-INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(9, 'Tran Van K',9,'0902123414',9);
-INSERT INTO `ManagerStudent`.`Students`(`id`,`fullname`,`address_id`,`phone`,`class_id`) VALUE(10, 'Tran Van J',10,'0902123415',10);
 
 INSERT INTO `ManagerStudent`.`Course`(`id`,`name`,`description`) VALUE (1, 'Tran Van A','abdcdadadfae');
 INSERT INTO `ManagerStudent`.`Course` (`id`, `name`, `description`) VALUES (2, 'Tran Van B', 'gsdfgfg');
@@ -79,13 +80,20 @@ INSERT INTO `ManagerStudent`.`Course` (`id`, `name`, `description`) VALUES (3, '
 INSERT INTO `ManagerStudent`.`Course` (`id`, `name`, `description`) VALUES (4, 'Tran Van D', 'gsdfgfg');
 INSERT INTO `ManagerStudent`.`Course` (`id`, `name`, `description`) VALUES (5, 'Tran Van E', 'gsdfgfg');
 
-INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES ('1', '1', '1', '5');
-INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES ('2', '2', '2', '6');
-INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES ('3', '3', '3', '8');
-INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES ('4', '4', '4', '8');
-INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES ('5', '5', '5', '7');
 
-SELECT COUNT(`id`) 'Tong Cac Hoc Vien Trong Lop' FROM `ManagerStudent`.`Classes`;
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES (1, 1, 1, 5);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES (2, 2, 2, 6);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES (3, 3, 3, 8);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES (4, 4, 4, 8);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `point`) VALUES (5, 5, 5, 7);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `points`) VALUES (6, 1, 1, 5);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `points`) VALUES (7, '2', 2,5);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `points`) VALUES (8, 3, 3, 6);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `points`) VALUES (9, 4, 4, 9);
+INSERT INTO `ManagerStudent`.`Point` (`id`, `course_id`, `student_id`, `points`) VALUES (10, 5, 5, 2);
+
+
+SELECT `name`,COUNT(`id`) 'Tong Cac Hoc Vien Trong Lop'FROM `ManagerStudent`.`Classes` GROUP BY `name`;
 
 SELECT COUNT(`id`) 'Tổng số học viên ở các tỉnh' FROM `ManagerStudent`.`Address`;
 
